ecd7092e0542556eec69375493750be87432a22e
Șerban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-03-26 09:57:32 +0200

    feat: base styles and remove default build

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[--bg] w-screen h-screen text-base font-[300] text-[--soft-blue]">
+      Equilibrium #3429
+
+      Our Equilibrium collection promotes balance and calm.
+
+      0.041 ETH
+      3 days left
+      
+      Creation of Jules Wyvern
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
